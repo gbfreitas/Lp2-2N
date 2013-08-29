170600cacf457594d13dcea9ac59531190fdad10
unknown <631110034@Sala602-08.fspoaeduc.com.br>
2013-08-29 19:34:12 -0300

    TRAVESSA DO DESERTO

---
+++ Jogo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Jogo{
+
+		int[] mapa = new int[10];
+		int[] tanque = new int[6];
+		static int P;
+		public static void main(String[] args){
+		System.out.println( "Travessia do Deserto\n " +
+					"O derserto tem 10 posições\n" +
+					"O caminhão tem que atrvessar o derserto.\n" +
+					"O caminhão pode levar no máximo 6 litros de tanque.\n" +
+					"Cada litro permite andar 1 posição.\n" +
+					"Na posição 0 o caminhão pode recarregar sempre.\n" +
+					"O caminhão pode descarregar em qualquer posição para carregar posteriormente.\n" +
+					"Na posição 9 o jogo acaba.\n");
+   
+		System.out.println("digite  numeros 0 a 9 para movimentar o caminhão: ");
+		Scanner teclado = new Scanner(System.in);
+		P = teclado.nextInt();
+		
+          System.out.println(P); 
+		}		
+	switch(P){
+	case 1: 
+		if(P==1){tanque=-1};
+	
+	case 2:
+		if(P==2{tanque=-2};)
+			
+	}
+}
+			
+	
